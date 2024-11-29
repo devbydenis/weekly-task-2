e48c99a85ae1091d75097126ee020ba7f9f537d6
denisrahmadi <hello.denisrahmadi@gmail.com>
2024-11-29 16:04:36 +0700

    soal nomor 1

---
+++ weekly-task-2-no-1.js
@@ -0,0 +1,29 @@
+// NO.1
+function fetchData(status) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (status === true) {
+                resolve("Data berhasil diambil");
+            } else {
+                reject("Gagal mengambil data");
+            }
+        }, 3000);
+    });
+}
+
+// then-catch
+fetchData(true)
+    .then((hasil) => console.log(`Hasil: ${hasil}`))
+    .catch((error) => console.log(`Error: ${error}`));
+
+// Async-Await with try catch
+async function prosesData() {
+    try {
+        const data = await fetchData(false);
+        console.log(`Hasil: ${data}`);
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+
+prosesData();
